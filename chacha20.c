44a0a18ec43bd2d203df5e9be93891287885bbd0
Pablo <pablogf@uma.es>
2024-09-26 12:44:53 +0200

    cleaned

--- chacha20.c
+++ chacha20.c
@@ -1,7 +1,6 @@
 # include <stdio.h>
 # include "chacha20_functions.h"
 
-
 int main()
 {
     // Load the initial state.
@@ -9,14 +8,12 @@ int main()
                     4, 5, 6, 7,
                     8, 9, 10, 11,
                     12, 13, 14, 15 };
-
     int i;
     
     for (i = 1; i <= 20; i++)
     {
         // Operate on columns for even rounds.
         if(i % 2 == 0){
-
             columns(state);
         }
 
