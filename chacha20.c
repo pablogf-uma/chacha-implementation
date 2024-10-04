91c1115b606f3f76c73b67cf0e9422bcc84b9100
Pablo <pablogf@uma.es>
2024-10-04 12:24:03 +0200

    little-endian display

--- chacha20.c
+++ chacha20.c
@@ -9,7 +9,7 @@ int main()
     // Parameters for the initial state:
     // Use of uint32_t (unsigned 32 int): Ensures that bit-wise operations are performed without sign extension
     uint32_t original_state[16];
-    char constant[16] = "apxe3 dnyb-2k et"; // Specified by the RFC, usually "expand 32-byte k"
+    char constant[16] = "expand 32-byte k";
     uint32_t key[8] = { 0x00000000, 0x00000000, 0x00000000, 0x00000000,
                     0x00000000, 0x00000000, 0x00000000, 0x00000000 };
     uint32_t blockcount = 0;
@@ -25,33 +25,42 @@ int main()
     // permute_block() function call:
     permute_block(state, constant, key, blockcount, nonce, state);
 
-    // Output state matrices:
-    printf("Original state matrix:\n");
+// Output state matrices:
+    printf("Original state matrix (little-endian):\n");
 
     for (int a = 0; a < 4; a++)
     {
         for (int b = 0; b < 4; b++)
         {
-            printf("%08x", original_state[a * 4 + b]); // Output in hexadecimal format
-            printf(" ");                              //  Relationship between 1D array and 4x4 matrix: rows * 4 + columns
+            // To output bytes in little endian, since printf() outputs them in big-endian
+            int x = original_state[a * 4 + b]; // Finding each element's index: row * 4 + column
+            unsigned char* ptr = (unsigned char*)&x; // Access the address of the integer
+            for(int i = 0; i < sizeof(int); i++)
+            {
+                printf("%02x", ptr[i]); // Output in hexadecimal format
+            }                           
+            printf(" ");                              
         }
         printf("\n");
     }
     
     printf("\n");
 
-    printf("Permuted state matrix:\n");
+    printf("Permuted state matrix (little-endian):\n");
     for (int e = 0; e < 4; e++)
     {
         for (int d = 0; d < 4; d++)
         {
-            printf("%08x", state[e * 4 + d]);
+            int y = state[e * 4 + d];
+            unsigned char* ptr2 = (unsigned char*)&y;
+            for(int i = 0; i < sizeof(int); i++)
+            {
+                printf("%02x", ptr2[i]);
+            }                           
             printf(" ");
         }
         printf("\n");
     }
-
     printf("\n");
-
     return 0;
 }
--- little_endian.c
+++
@@ -1,7 +0,0 @@
-# include <stdio.h>
-# include <stdint.h>
-
-uint32_t little_endian(uint32_t *number)
-{
-
-}
